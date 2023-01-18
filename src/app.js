c6078b6d7a5b92420f168bd6eebd2d0aa9f0524e
GustavoFFLima <frazao39@gmail.com>
2023-01-18 14:56:52 -0300

    configuração inicial

--- src/app.js
+++ src/app.js
@@ -1,1 +1,14 @@
-import
+import express from 'express'
+import cors from 'cors'
+import { MongoClient } from 'mongodb'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+const PORT = 5000
+
+app.listen(PORT, () => console.log('Servidor deu certo!'))
